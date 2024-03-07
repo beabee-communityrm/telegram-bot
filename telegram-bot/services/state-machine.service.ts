7ebc865205deee621974bd81cf68a95b58746f4e
Pascal Garber <pascal@artandcode.studio>
2024-03-07 17:23:49 +0100

    chore(state-machine): Basic implementation of the state machine using valtio

--- telegram-bot/services/state-machine.service.ts
+++ telegram-bot/services/state-machine.service.ts
@@ -1,10 +1,94 @@
 import { BaseService } from "../core/index.ts";
-import { ContentClient, Singleton } from "../deps.ts";
-
-import type { Content, ContentId } from "../types/index.ts";
+import { proxy, ref, Singleton, snapshot, subscribe, watch } from "../deps.ts";
 
 @Singleton()
 export class StateMachineService extends BaseService {
+  /**
+   * Create a new proxy state object.
+   * The [proxy](https://valtio.pmnd.rs/docs/api/basic/proxy) tracks changes to the original object and all nested objects, notifying listeners when an object is modified.
+   * @param baseObject
+   * @returns
+   */
+  public create<T extends object>(baseObject?: T): T {
+    return proxy(baseObject);
+  }
+
+  /**
+   * [Subscribe](https://valtio.pmnd.rs/docs/api/advanced/subscribe) to changes in the state.
+   * @example
+   * ```ts
+   * const state = stateMachine.create({ count: 0 });
+   * stateMachine.subscribe(state, (value) => console.log(value.count));
+   * state.count++;
+   * ```
+   *
+   * You can also subscribe to a portion of state.
+   * @example
+   * ```ts
+   * const state = stateMachine.create(({ obj: { foo: 'bar' }, arr: ['hello'] });
+   * stateMachine.subscribe(.obj, () => console.log('state.obj has changed to', state.obj));
+   * state.count++;
+   * ```
+   * @param proxy
+   * @param callback
+   * @returns
+   */
+  public subscribe = subscribe;
+
+  /**
+   * [snapshot](https://valtio.pmnd.rs/docs/api/advanced/snapshot) takes a proxy and returns an immutable object, unwrapped from the proxy.
+   * Immutability is achieved by *efficiently* deep copying & freezing the object.
+   *
+   * @example
+   * ```ts
+   * const store = stateMachine.create({ name: 'Mika' })
+   * const snap1 = stateMachine.snapshot(store) // an efficient copy of the current store values, unproxied
+   * const snap2 = stateMachine.snapshot(store)
+   * console.log(snap1 === snap2) // true, no need to re-render
+   *
+   * store.name = 'Hanna'
+   * const snap3 = stateMachine.snapshot(store)
+   * console.log(snap1 === snap3) // false, should re-render
+
+   * ```
+   */
+  public snapshot = snapshot;
+
+  /**
+   * A [ref](https://valtio.pmnd.rs/docs/api/advanced/ref) allows unproxied state in a proxy state.
+   * A `ref` is useful in the rare instances you to nest an object in a `proxy` that is not wrapped in an inner proxy and, therefore, is not tracked.
+   * @example
+   * ```ts
+   * const store = stateMachine.create({
+   *     users: [
+   *         { id: 1, name: 'Juho', uploads: ref([]) },
+   *     ]
+   *   })
+   * })
+   * ```
+   * Once an object is wrapped in a ref, it should be mutated without resetting the object or rewrapping in a new ref.
+   * @example
+   * ```ts
+   * // do mutate
+   * store.users[0].uploads.push({ id: 1, name: 'Juho' })
+   * // do reset
+   * store.users[0].uploads.splice(0)
+   *
+   * // don't
+   * store.users[0].uploads = []
+   * ```
+   * A ref should also not be used as the only state in a proxy, making the proxy usage pointless.
+   */
+  public ref = ref;
+
+  /**
+   * subscription via a getter.
+   * [watch](https://valtio.pmnd.rs/docs/api/utils/watch) supports subscribing to multiple proxy objects (unlike `subscribe` which listens to only a single proxy). Proxy objects are subscribed with a `get` function passed to the callback.
+   *
+   * Any changes to the proxy object (or its child proxies) will rerun the callback.
+   */
+  public watch = watch;
+
   constructor() {
     super();
   }
